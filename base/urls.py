35c09fa039bbd11641b60d344004324f8d25efe1
mohab fouda <153132608+MohabFouda20@users.noreply.github.com>
2023-12-11 01:57:20 +0200

    adding a template

---
+++ base/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.home , name= "name" ),
+    path ('king/',views.king  , name="king")
+]
--- project/urls.py
+++ project/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path , include
 from django.http import HttpResponse
-def home(request):
-    return HttpResponse('home page')
+
     
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home)
+    path ('', include('base.urls')),
     
 ]
